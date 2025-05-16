88ecf4de5dda9bc3831ca327d3f657ec0c02c6ce
tamilatiurina <tiurinatamila@gmail.com>
2025-05-16 11:06:59 +0200

    get api departures and jobs

--- src/Tutorial10.RestAPI/Program.cs
+++ src/Tutorial10.RestAPI/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Tutorial10.RestAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var _connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<_2019sbdContext>(options => options.UseSqlServer(_connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,16 +20,40 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/jobs", () => {
-    
-});
-
-app.MapGet("/api/departments", () => {
-    
-});
-
-app.MapGet("/api/employees", () =>
+app.MapGet("/api/jobs", async (_2019sbdContext context, CancellationToken token) =>
 {
+    try
+    {
+        return Results.Ok(await context.Jobs.ToListAsync(token));
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapGet("/api/departments",async (_2019sbdContext context, CancellationToken token) => {
+    
+    try
+    {
+        return Results.Ok(await context.Departemnts.ToListAsync(token));
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapGet("/api/employees",async (_2019sbdContext context, CancellationToken token) => 
+{
+    try
+    {
+        return Results.Ok(await context.Employees.ToListAsync(token));
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
     
 });
 
